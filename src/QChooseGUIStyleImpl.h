c0f989a917ffade812816af8320faa619ad0e3ab
gauthier.brion <gauthier.brion@c4805ffc-1a53-0410-82a6-f709db69df85>
2008-10-25 21:17:15 +0000

    Added the possibility to choose visual style

---
+++ src/QChooseGUIStyleImpl.h
@@ -0,0 +1,28 @@
+#ifndef QCHOOSEGUISTYLEIMPL_H
+#define QCHOOSEGUISTYLEIMPL_H
+//
+#include <QDialog>
+#include "ui_QChooseGUIStyle.h"
+
+#include <QStyleFactory>
+//
+class QChooseGUIStyleImpl : public QDialog, public Ui::QChooseGUIStyle
+{
+	Q_OBJECT
+	
+public:
+	QChooseGUIStyleImpl( QWidget * parent = 0, Qt::WFlags f = 0 );
+	
+private slots:
+	void on_keepButton_clicked();
+	void on_tryButton_clicked();
+	void on_quitButton_clicked();
+	
+private:
+	QString beforeStyle;
+};
+#endif
+
+
+
+
